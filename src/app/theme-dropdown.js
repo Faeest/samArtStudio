229ef53949039b152754723255e6db5279def734
Faeest <faeest.115@gmail.com>
2025-06-09 20:58:04 +0700

    feat: setting the page and the themes

---
+++ src/app/theme-dropdown.js
@@ -0,0 +1,33 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuPortal, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuSub, DropdownMenuSubContent, DropdownMenuSubTrigger, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { IconCircleFilled } from "@tabler/icons-react";
+import { themes } from "./themes";
+import { useTheme } from "next-themes";
+import { cn } from "@/lib/utils";
+function CheckerBadge({ isActive }) {
+	return <IconCircleFilled className={cn("ml-auto size-3 text-primary opacity-0 transition-opacity", isActive && "opacity-100")} />;
+}
+export function ThemeDropdown() {
+	const { theme, setTheme } = useTheme();
+	const isActive = (value) => theme === value;
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant="outline">Open</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className="w-56" align="start">
+				<DropdownMenuGroup>
+					{themes.slice(3).map((theme) => (
+						<DropdownMenuItem key={theme.value} onClick={() => setTheme(theme.value)}>
+							<theme.icon className="size-4 shrink-0" />
+							<span className="truncate">{theme.value}</span>
+							<CheckerBadge isActive={isActive(theme.value)} />
+						</DropdownMenuItem>
+					))}
+				</DropdownMenuGroup>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+}
---
+++ src/app/themes.js
@@ -0,0 +1,106 @@
+import { IconBrandTwitter } from "@tabler/icons-react";
+import { AsteriskIcon, BeanIcon, CandyIcon, CatIcon, CitrusIcon, CloudyIcon, CoffeeIcon, CpuIcon, Disc3Icon, EclipseIcon, HeartIcon, HourglassIcon, JapaneseYenIcon, LaptopIcon, LeafIcon, MoonIcon, NotebookPenIcon, RabbitIcon, SparkleIcon, SunIcon, SunsetIcon, TreePineIcon, TvIcon, WindIcon } from "lucide-react";
+
+export const themes = [
+	{
+		value: "light",
+		icon: SunIcon,
+	},
+	{
+		value: "dark",
+		icon: MoonIcon,
+	},
+	{
+		value: "system",
+		icon: LaptopIcon,
+	},
+	// Custom
+	{
+		value: "bean",
+		icon: BeanIcon,
+	},
+	{
+		value: "bubblegum",
+		icon: CandyIcon,
+	},
+	{
+		value: "caffeine",
+		icon: CoffeeIcon,
+	},
+	{
+		value: "claude",
+		icon: AsteriskIcon,
+	},
+	{
+		value: "cosmic",
+		icon: SparkleIcon,
+	},
+	{
+		value: "cute",
+		icon: RabbitIcon,
+	},
+	{
+		value: "dreamy",
+		icon: CloudyIcon,
+	},
+	{
+		value: "ghibli",
+		icon: CatIcon,
+	},
+	{
+		value: "nature",
+		icon: LeafIcon,
+	},
+	{
+		value: "neo",
+		icon: CpuIcon,
+	},
+	{
+		value: "notebook",
+		icon: NotebookPenIcon,
+	},
+	{
+		value: "perpetuity",
+		icon: HourglassIcon,
+	},
+	{
+		value: "pinky",
+		icon: HeartIcon,
+	},
+	{
+		value: "retro",
+		icon: TvIcon,
+	},
+	{
+		value: "solar",
+		icon: EclipseIcon,
+	},
+	{
+		value: "sunset",
+		icon: SunsetIcon,
+	},
+	{
+		value: "tangerine",
+		icon: CitrusIcon,
+	},
+	{
+		value: "tokyo",
+		icon: JapaneseYenIcon,
+	},
+	{
+		value: "tree",
+		icon: TreePineIcon,
+	},
+	{
+		value: "twitter",
+		icon: IconBrandTwitter,
+	},
+	{
+		value: "vintage",
+		icon: Disc3Icon,
+	},
+	{
+		value: "windy",
+		icon: WindIcon,
+	},
+];
